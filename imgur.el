f4e77babc8e0c41bfb226d153a910b383004ed6d
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2024-10-13 08:44:17 +0200

    [ADD] Initial package skeleton

---
+++ imgur.el
@@ -0,0 +1,36 @@
+;;; imgur.el --- Imgur client -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024 Peter Badida
+
+;; Author: Peter Badida <keyweeusr@gmail.com>
+;; Keywords: convenience, imgur, client
+;; Version: 1.0.0
+;; Package-Requires: ((emacs "24.1"))
+;; Homepage: https://github.com/KeyWeeUsr/imgur
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; TBD
+
+;;; Code:
+
+(defgroup imgur nil
+  "Imgur client configuration."
+  :group 'external
+  :group 'communication)
+
+(provide 'imgur)
+;;; imgur.el ends here
